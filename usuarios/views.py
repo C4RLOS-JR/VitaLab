1d838d36da0fffd46a20f2891befaee73e908c38
C4RLOS-JR <camjr3z@gmail.com>
2024-05-30 12:12:30 -0300

    CArlos

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 def cadastro(request):
-  return render(request, 'cadastro.html')
+
+  if request.method == 'GET':
+    return render(request, 'cadastro.html')
+  
+  elif request.method == 'POST':
+    primeiro_nome = request.POST.get('primeiro_nome')
+    segundo_nome = request.POST.get('segundo_nome')
+    username = request.POST.get('username')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    confirmar_senha = request.POST.get('confirmar_senha')
+
+    if not senha == confirmar_senha:
+      return redirect('/usuarios/cadastro')
+
+    return HttpResponse(f'{username} - {email}')
