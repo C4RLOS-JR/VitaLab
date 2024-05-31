20c40108e5596652c32230d71020ef5b2e33165a
C4RLOS-JR <camjr3z@gmail.com>
2024-05-31 19:56:22 -0300

    Carlos

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,9 @@
+from logging import ERROR
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def cadastro(request):
 
@@ -8,13 +12,35 @@ def cadastro(request):
   
   elif request.method == 'POST':
     primeiro_nome = request.POST.get('primeiro_nome')
-    segundo_nome = request.POST.get('segundo_nome')
+    ultimo_nome = request.POST.get('ultimo_nome')
     username = request.POST.get('username')
     email = request.POST.get('email')
     senha = request.POST.get('senha')
     confirmar_senha = request.POST.get('confirmar_senha')
 
     if not senha == confirmar_senha:
+      messages.add_message(request, constants.ERROR, 'As senhas não são iguais!')
+      return redirect('/usuarios/cadastro')
+    
+    if len(senha) < 6:
+      messages.add_message(request, constants.ERROR, 'A senha precisa ter no mínimo 6 caracteres!')
+      return redirect('/usuarios/cadastro')
+    
+    if User.objects.filter(username=username):
+      messages.add_message(request, constants.ERROR, 'Esse nome de usuário já existe!')
       return redirect('/usuarios/cadastro')
 
-    return HttpResponse(f'{username} - {email}')
+    try:
+      usuario = User.objects.create_user(
+        first_name=primeiro_nome,
+        last_name=ultimo_nome,
+        username=username,
+        email=email,
+        password=senha
+      )
+    except:
+      messages.add_message(request, constants.ERROR, 'Erro interno do sistema!')
+      return redirect('/usuarios/cadastro')
+
+    messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso!')
+    return redirect('/usuarios/cadastro')
