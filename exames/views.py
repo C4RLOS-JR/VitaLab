7043a32ce14118173703cdbdcd4841933c4db0ca
C4RLOS-JR <camjr3z@gmail.com>
2024-06-02 20:16:36 -0300

    Carlos

--- exames/views.py
+++ exames/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def solicitar_exames(request):
+  if request.method == 'GET':
+    return render(request, 'solicitar_exames.html')
