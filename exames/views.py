39b2d9ee711db5188ab2e8f9dce9c43cc830db93
C4RLOS-JR <camjr3z@gmail.com>
2024-06-05 20:41:17 -0300

    Carlos

--- exames/views.py
+++ exames/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import TiposExames, SolicitacaoExames
+from .models import TiposExames, PedidosExames, SolicitacaoExames
+from datetime import datetime
+import calendar
 
 @login_required
 def solicitar_exames(request):
@@ -11,16 +13,28 @@ def solicitar_exames(request):
     return render(request, 'solicitar_exames.html', {'exames': exames})
   elif request.method == 'POST':
     exames_id = request.POST.getlist('exames_selecionados')
-    exames_solicitados = TiposExames.objects.filter(id__in=exames_id)
-    
-    # Fazer : Calcular o preço somente dos exames disponíveis.
+    exames_solicitados = TiposExames.objects.filter(id__in=exames_id)    
     preco_total = 0
-    for exame in exames_solicitados:
-      preco_total += exame.preco
+    data = datetime.now()
 
+    for exame in exames_solicitados:
+      if exame.disponivel:
+        preco_total += exame.preco
 
     return render(request, 'solicitar_exames.html', {
       'exames': exames,
       'exames_solicitados': exames_solicitados,
-      'preco_total':preco_total
+      'preco_total':preco_total,
+      'data': data
     })
+
+def fechar_pedido(request):
+  exames_id = request.POST.getlist('exames')
+
+  pedido_exame = PedidosExames(
+    usuario = request.user,
+    data = datetime.now()
+  )
+  pedido_exame.save()
+
+  return HttpResponse('teste')
