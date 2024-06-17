597d66b62fb5fc89f6753e1326fccd47a27bfa0f
C4RLOS-JR <camjr3z@gmail.com>
2024-06-17 20:06:02 -0300

    Carlos

--- exames/admin.py
+++ exames/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import PedidosExames, SolicitacaoExames, TiposExames
+from .models import PedidosExames, SolicitacaoExames, TiposExames, AcessoMedico
 
 admin.site.register(TiposExames)
 admin.site.register(SolicitacaoExames)
 admin.site.register(PedidosExames)
+admin.site.register(AcessoMedico)
