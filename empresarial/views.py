02d893f9b6b36a26a9e759eb97bab660632403a2
C4RLOS-JR <camjr3z@gmail.com>
2024-07-12 15:25:05 -0300

    Carlos (final)

--- empresarial/views.py
+++ empresarial/views.py
@@ -1,4 +1,3 @@
-from asyncio import constants
 from django.shortcuts import redirect, render
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpResponse, FileResponse
@@ -8,6 +7,7 @@ from django.contrib.auth.models import User
 from django.db.models.functions import Concat
 from django.db.models import Value
 from exames.models import SolicitacaoExames
+from .utils import gerar_pdf_exames, gerar_senha_aleatoria
 
 
 @staff_member_required # Se for membro da equipe
@@ -41,3 +41,33 @@ def proxy_pdf(request, exame_id):
   resultado = exame.resultado.open()
 
   return HttpResponse(resultado)
+
+def gerar_senha(request, exame_id):
+  exame = SolicitacaoExames.objects.get(id=exame_id)
+
+  if not exame.senha:
+    exame.senha = gerar_senha_aleatoria(6)
+    exame.save()
+    
+  return FileResponse(gerar_pdf_exames(exame, exame.usuario, exame.senha), filename=f'{exame.usuario.first_name} - {exame.exame.nome}.pdf')
+
+def alterar_dados_exame(request, exame_id):
+  exame = SolicitacaoExames.objects.get(id=exame_id)
+  pdf = request.FILES.get('resultado')
+  status = request.POST.get('status')
+  requer_senha = request.POST.get('requer_senha')
+
+  if requer_senha and (not exame.senha):
+    messages.add_message(request, constants.ERROR, 'Para exigir a senha é preciso gerar uma!')
+    return redirect(f'/empresarial/exame_cliente/{exame_id}')
+
+  if pdf:
+    exame.resultado = pdf
+  
+  exame.status = status
+  exame.requer_senha = True if requer_senha else False
+
+  exame.save()
+  messages.add_message(request, constants.SUCCESS, 'Dados alterados com sucesso!')
+
+  return redirect(f'/empresarial/exame_cliente/{exame_id}')
